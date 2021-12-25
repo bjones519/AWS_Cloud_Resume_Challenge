f69465bc0298aeae89bff738fd544a66eab85404
Brittney <brittney.r.jones519@gmail.com>
2021-12-24 23:12:21 -0500

    initial commit

---
+++ html5up-strata/test.js
@@ -0,0 +1,27 @@
+/*let person = 'Brit'
+
+function homeGreet(){
+    let name = prompt("What is your name?");
+    return alert(`Hi ${name}`);
+    
+}
+
+// anonymous function
+let greet = function (){
+    var person = 'Brittney';
+    return `Hi ${person}`;
+};
+
+
+//arrow function expression
+let greet2 = () =>{
+    var person = 'Brittney';
+    return `Hi ${person}`;
+};
+
+
+//alert(greet());
+alert(greet2());
+//homeGreet()
+alert(person)
+*/
